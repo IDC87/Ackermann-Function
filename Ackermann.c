e330f4abfb458e3217151e3ccd5e49a78b7948f9
Ivo <ivo.diogo@gmail.com>
2021-10-31 14:15:27 +0000

    some emends

--- Ackermann.c
+++ Ackermann.c
@@ -11,10 +11,13 @@ int ack(int m, int n)
     else if((m > 0) && (n > 0)){
         return ack(m-1, ack(m, n-1));
     }
+
+    return 0;
 }
  
 int main(int argc, char **argv)
 {
+    //Limite fica nos valores A(4, 2) que resulta em 2×10^19728 - impossivel de ser executado
     int m;
     int n;
     int A;
